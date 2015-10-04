fbbe666b1398b39c9f4ea44283a278cadc25bae3
sjau <github.com@sjau.ch>
2015-10-04 16:47:26 +0200

    Fixed missing if

--- pastesl.nix
+++ pastesl.nix
@@ -1,10 +1,10 @@
-{stdenv }:
+{stdenv, fetchgit }:
 stdenv.mkDerivation {
   name = "pastesl-git";
   src = fetchgit {
-    url = https://github.com/sjau/pastesl;
-    rev = "d4a292bfe369b934e4d014bf58a20e25cf7f2bb4";
-    sha256 = "0giy09fwsdv35801sqrjbrihql88fksk0bl4w351wxibzhgn1vsa";
+    url = https://github.com/sjau/pastesl.git;
+    rev = "09c8b961f4373fe4ace0d21f73c7bbf12d659eb6";
+    sha256 = "0d2xvrrz70w3k92skgp59wwpclxcf518kvh6z201n6vsdjjqrxhd";
   };
   installPhase = ''
     mkdir -p $out/bin
