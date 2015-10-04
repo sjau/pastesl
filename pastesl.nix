178137c1b98caf610074dba6d3f03063a0ccd1ec
sjau <github.com@sjau.ch>
2015-10-04 16:51:29 +0200

    updating rev and sha for nix expression

--- pastesl.nix
+++ pastesl.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation {
   name = "pastesl-git";
   src = fetchgit {
     url = https://github.com/sjau/pastesl.git;
-    rev = "09c8b961f4373fe4ace0d21f73c7bbf12d659eb6";
-    sha256 = "0d2xvrrz70w3k92skgp59wwpclxcf518kvh6z201n6vsdjjqrxhd";
+    rev = "fbbe666b1398b39c9f4ea44283a278cadc25bae3";
+    sha256 = "1jg1cv6miwj4ir9li5i8qj5mhag0kai7r5rvjjq5hwga6dgh3clr";
   };
   installPhase = ''
     mkdir -p $out/bin
