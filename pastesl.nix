3f0300056c92da19f67fd24d6d574e0e11d696e5
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2024-11-23 15:47:13 +0100

    Update pastesl.nix

--- pastesl.nix
+++ pastesl.nix
@@ -3,8 +3,8 @@ stdenv.mkDerivation {
   name = "pastesl-git";
   src = fetchgit {
     url = https://github.com/sjau/pastesl.git;
-    rev = "c133c0241d4c41b99b99dc01c5037f1c6d23bec3";
-    sha256 = "0vk8dzjd947cnkvhnfzavm8y2421hrm17kss6qaky59rliaxh095";
+    rev = "d9eefa4667a098cfa924641bdd6d6a9a31ff35d2";
+    sha256 = "0vk8dzjd947cnkvhnfzavm8y2421hrm17kss6qaky59rliaxh000";
   };
   installPhase = ''
     mkdir -p $out/bin
