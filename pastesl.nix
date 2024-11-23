730fb9c5c0e81f058ae45d7a8e50a27c3641a7dc
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2024-11-23 15:51:55 +0100

    Update pastesl.nix

--- pastesl.nix
+++ pastesl.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = https://github.com/sjau/pastesl.git;
     rev = "d9eefa4667a098cfa924641bdd6d6a9a31ff35d2";
-    sha256 = "YTngMRqaxwdrsxXk8oHq2Ji2sb9ruCkUadv75pUGg8s=";
+    sha256 = "sha256-YTngMRqaxwdrsxXk8oHq2Ji2sb9ruCkUadv75pUGg8s=";
   };
   installPhase = ''
     mkdir -p $out/bin
