b6707e9b3766ed27dc88704aa90b28dcd45b1cb3
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2024-11-26 17:45:58 +0100

    Update pastesl.nix

--- pastesl.nix
+++ pastesl.nix
@@ -1,20 +1,21 @@
-{stdenv, curl, jq, fetchgit }:
+{stdenv, curl, fetchgit, jq }:
 stdenv.mkDerivation {
   name = "pastesl-git";
   src = fetchgit {
     url = https://github.com/sjau/pastesl.git;
     rev = "a9cf366a97f0f60d8a7e50175e4c2f0e97beafb2";
-    sha256 = "sha256-YTngMRqaxwdrsxXk8oHq2Ji2sb9ruCkUadv75pUGg899";
+    sha256 = "sha256-vjtRACMeYQbj7ofHdcLH2OvCPV7bWByU+psnsWuAsMQ=";
   };
+  buildInputs = [ curl jq ];
   installPhase = ''
     mkdir -p $out/bin
     cp -n pastesl $out/bin/
 
     for i in $out/bin/*; do
       substituteInPlace $i \
-        --replace curl ${curl}/bin/curl
+        --replace curl ${curl}/bin/curl \
         --replace jq ${jq}/bin/jq
     done
-
   '';
+
 }
